3394ea2dcd1c4a9d403e4f737b37565cee1ca4dc
techbeastvishal <58232073+techbeastvishal@users.noreply.github.com>
2019-11-26 19:53:03 +0000

    Add files via upload

---
+++ kanban-eureka-server/src/test/java/com/techbeast/kanban/eureka/server/KanbanEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbeast.kanban.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
