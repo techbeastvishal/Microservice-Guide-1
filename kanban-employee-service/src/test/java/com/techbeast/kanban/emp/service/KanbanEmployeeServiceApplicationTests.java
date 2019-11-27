f56c0d74d9c9ccb15a57b219c176649548139e25
techbeastvishal <58232073+techbeastvishal@users.noreply.github.com>
2019-11-27 19:31:04 +0000

    updated testing code

--- kanban-employee-service/src/test/java/com/techbeast/kanban/emp/service/KanbanEmployeeServiceApplicationTests.java
+++ kanban-employee-service/src/test/java/com/techbeast/kanban/emp/service/KanbanEmployeeServiceApplicationTests.java
@@ -1,13 +1,15 @@
 package com.techbeast.kanban.emp.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class KanbanEmployeeServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+    public void contextLoads() throws Exception {
+    }
 }
